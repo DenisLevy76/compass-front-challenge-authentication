8fb090c608a1c4cb6ad9e4dac03f51ca65e61695
Denis Levy <denis.levi11@gmail.com>
2022-10-13 13:59:09 -0300

    Add login function and readme file

---
+++ scripts/login.js
@@ -0,0 +1,14 @@
+const formRef = document.querySelector('.signIn');
+const userInput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
+
+const login = (event) => {
+  event.preventDefault();
+  const user = userInput.value;
+  const password = passwordInput.value;
+
+  if (user === 'admin@admin.com.br' && password === 'admin') {
+    formRef.classList.remove('error');
+    window.location.replace('https://google.com');
+  } else formRef.classList.add('error');
+};
